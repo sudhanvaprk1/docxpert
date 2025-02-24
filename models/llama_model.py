744e2b32d3c44915f815397415c8c97709b9b0cd
Sudhanva Kulkarni <sudhanvaprk1@gmail.com>
2025-02-24 20:57:38 +0530

    deleting unnecessary files and folders

--- models/llama_model.py
+++ models/llama_model.py
@@ -1,23 +1,7 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from app.config import Config
+import ollama
 
 config = Config()
-
-# class LlamaModel:
-#     def __init__(self):
-#         self.tokenizer = AutoTokenizer.from_pretrained(config.LLAMA_MODEL_PATH, 
-#                                                         use_auth_token=config.LLAMA_API_KEY)
-#         self.model = AutoModelForCausalLM.from_pretrained(config.LLAMA_MODEL_PATH, 
-#                                                       use_auth_token=config.LLAMA_API_KEY)
-
-#     def generate(self, prompt, max_length=500):
-#         inputs = self.tokenizer(prompt, return_tensors="pt", truncation=True)
-#         outputs = self.model.generate(inputs["input_ids"], max_new_tokens=max_length)
-#         response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-#         return response
-
-
-import ollama
 
 class LlamaModel:
     def __init__(self, model_name="llama3"):
